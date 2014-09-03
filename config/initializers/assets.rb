12d03cdef3f6a2fd04f26bd4d2e5f9efbe2be3aa
Srdjan Prodanovič <s1e@cuf.si>
2014-09-03 21:38:29 +0200

    Configure assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += ['lib/*', 'models/*', 'views/*', 'app.js', 'app.css', 'lib/*.css']
