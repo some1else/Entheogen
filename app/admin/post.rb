50b73689745b16553eff828c2c0828d1f9a9ff70
Srdjan Prodanovič <s1e@cuf.si>
2014-09-12 12:58:47 +0200

    Published at

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Post do
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # permit_params :list, :of, :attributes, :on, :model
-  permit_params :body
+  permit_params :body, "published_at(1i)", "published_at(2i)", "published_at(3i)", "published_at(4i)", "published_at(5i)"
   #
   # or
   #
