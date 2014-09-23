ddfc3b631f6bd0689b0fc8ae06b08548a78cd138
Srdjan Prodanovič <s1e@cuf.si>
2014-09-23 21:55:54 +0200

    Gallery_id should be a permitted_param on Event. Body should be permitted+param on Entheogenesis

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Event do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :name, "begins_at(1i)", "begins_at(2i)", "begins_at(3i)", "begins_at(4i)", "begins_at(5i)", :venue, :lineup, :fb_link
+  permit_params :name, "begins_at(1i)", "begins_at(2i)", "begins_at(3i)", "begins_at(4i)", "begins_at(5i)", :venue, :lineup, :fb_link, :gallery_id
 
   #
   # or
